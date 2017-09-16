2adfe8bb209d136dd368eb063883c458ec36a239
dengsang <dengsangt@gmail.com>
2017-09-16 13:52:31 +0300

    update dashbord api test on specific items to be updated and deleted

--- shopping_api_test.py
+++ shopping_api_test.py
@@ -12,7 +12,7 @@ class ShoppingListApiTestCase(unittest.TestCase):
         self.app = create_app(config_name="testing")
         self.client = self.app.test_client
         self.shopping_list = {'item': 'Items in my shopping list'}
-        self.shopping_app_user = {'username': 'my name is lovren'}
+        # self.shopping_app_user = {'username': 'my name is charles'}
 
         # create all tables
         with self.app.app_context():
@@ -35,12 +35,12 @@ class ShoppingListApiTestCase(unittest.TestCase):
 
         """Test API can get a single shopping list by using it's id."""
 
-    def test_api_can_get_shopping_list_by_id(self):
+    def test_api_can_get_shopping_list_by_item(self):
         rv = self.client().post('/dashboard/', data=self.shopping_list)
         self.assertEqual(rv.status_code, 201)
         result_in_json = json.loads(rv.data.decode('utf-8').replace("'", "\""))
         result = self.client().get(
-            '/dashboard/{}'.format(result_in_json['id']))
+            '/dashboard/{}'.format(result_in_json['item']))
         self.assertEqual(result.status_code, 200)
         self.assertIn('Check shopping list', str(result.data))
 
@@ -57,7 +57,7 @@ class ShoppingListApiTestCase(unittest.TestCase):
                 "item": "Items in the list can be added or removed"
             })
         self.assertEqual(rv.status_code, 200)
-        results = self.client().get('/dashboard/1')
+        results = self.client().get('/dashboard/<item>')
         self.assertIn('Be checking the list ', str(results.data))
 
     """Test API can delete an existing shopping list. (DELETE request)."""
@@ -67,11 +67,11 @@ class ShoppingListApiTestCase(unittest.TestCase):
             '/dashboard/',
             data={'item': 'carrots, chocolate and maize'})
         self.assertEqual(rv.status_code, 201)
-        res = self.client().delete('/dashboard/1')
+        res = self.client().delete('/dashboard/<item>')
         self.assertEqual(res.status_code, 200)
 
         # Test to see if it exists, should return a 404
-        result = self.client().get('/dashboard/1')
+        result = self.client().get('/dashboard/<item>')
         self.assertEqual(result.status_code, 404)
 
     """teardown all initialized variables."""
