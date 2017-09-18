181d42512e015389b2a830e635e29e855e949d70
dengsang <dengsangt@gmail.com>
2017-09-18 10:38:23 +0300

    Add the POST, GET ,PUT DELETE and PATCH module for the  models.User class that handles the app user accounts

--- api/__init__.py
+++ api/__init__.py
@@ -4,6 +4,7 @@ from flask import request, jsonify, abort
 
 from instance.config import app_config
 from api.models import ShoppingListApi
+from api.models import User
 
 # initialize sql-alchemy
 db = SQLAlchemy()
@@ -26,6 +27,7 @@ def create_app(config_name):
                                                 price=request.data.price)
                 shopping_list.save()
                 response = jsonify({
+                    'id': shopping_list.id,
                     'item': shopping_list.item,
                     'quantity': shopping_list.quantity,
                     'price': shopping_list.price,
@@ -41,6 +43,7 @@ def create_app(config_name):
 
             for shopping_list in shoppings:
                 obj = {
+                    'id': shopping_list.id,
                     'item': shopping_list.item,
                     'quantity': shopping_list.quantity,
                     'price': shopping_list.price,
@@ -67,8 +70,8 @@ def create_app(config_name):
                    }, 200
 
         elif request.method == 'PUT':
-            items = str(request.data.get(item='', quantity='', price=''))
-            shopping_list.name = items
+            item = str(request.data.get(item='', quantity='', price=''))
+            shopping_list.item = item
             shopping_list.save()
             response = jsonify({
                 'id': shopping_list.id,
@@ -89,6 +92,78 @@ def create_app(config_name):
                 'price': shopping_list.price,
                 'date_created': shopping_list.date_created,
                 'date_modified': shopping_list.date_modified
+            })
+            response.status_code = 200
+            return response
+
+    """User Sign Up Api section"""
+    @app.route('/signup/', methods=['POST', 'GET'])
+    def signup():
+        if request.method == "POST":
+            users = str(request.data.get(email='', username='', password=''))
+            if users:
+                user = User(email=request.data.email, username=request.data.username,
+                            password=request.data.password)
+                user.save()
+                response = jsonify({
+                    'id': user.id,
+                    'email': user.email,
+                    'username': user.username,
+                    'password': user.password
+                })
+                response.status_code = 201
+                return response
+        else:
+            # GET
+            app_users = User.get_all()
+            results = []
+
+            for user in app_users:
+                obj = {
+                    'id': user.id,
+                    'email': user.email,
+                    'username': user.username,
+                    'password': user.password
+                }
+                results.append(obj)
+            response = jsonify(results)
+            response.status_code = 200
+            return response
+
+    @app.route('/signup/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+    def signup_manipulation(id):
+        # retrieve  the user accounts using it's ID
+        user = User.query.filter_by(id=id).first()
+        if not user:
+            # Raise an Exception with a 404 not found status code
+            abort(404)
+
+        if request.method == 'DELETE':
+            user.delete()
+            return {
+                       "message": "Account {} deleted successfully".format(user.id)
+                   }, 200
+
+        elif request.method == 'PUT':
+            user_details = str(request.data.get(email='', username='', password=''))
+            user.email = user_details
+            user.username = user_details
+            user.save()
+            response = jsonify({
+                'id': user.id,
+                'email': user.email,
+                'username': user.username,
+                'password': user.password
+            })
+            response.status_code = 200
+            return response
+        else:
+            # GET
+            response = jsonify({
+                'id': user.id,
+                'email': user.email,
+                'username': user.username,
+                'password': user.password
             })
             response.status_code = 200
             return response
